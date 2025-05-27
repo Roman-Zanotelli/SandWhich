64256f0b172d5cf143812f147023182e4b8ca7b8
Roman-Zanotelli <Roman-Zanotelli@github.com>
2025-05-27 14:15:01 -0400

    Generated Test Main File Automatically

--- SandWhich/expresso-processor/src/main/java/com/pluralsight/ExpressoProcessor.java
+++ SandWhich/expresso-processor/src/main/java/com/pluralsight/ExpressoProcessor.java
@@ -6,6 +6,9 @@ import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Set;
 
 @SupportedAnnotationTypes("*")
@@ -15,12 +18,32 @@ public class ExpressoProcessor extends AbstractProcessor {
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.MANDATORY_WARNING, ">> Expresso Processor Invoked");
     }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        if (roundEnv.processingOver()) {
+            return false;
+        }
         processingEnv.getMessager().printMessage(Diagnostic.Kind.MANDATORY_WARNING, ">> Expresso Processor Invoked");
+        try {
+            JavaFileObject file = processingEnv.getFiler().createSourceFile("com.pluralsight.generated.ExpressoMain");
+            try (Writer writer = file.openWriter()) {
+                writer.write("""
+                package com.pluralsight.generated;
+                
+                public class ExpressoMain {
+                    public static void main(String[] args) {
+                        System.out.println("Hello from generated main!");
+                    }
+                }
+                """);
+
+            }
+
+        } catch (IOException e) {
+
+        }
         return true;
     }
 }
